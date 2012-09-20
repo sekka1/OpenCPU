c7b118a57e19e205de300746068aea4a30c902aa
Rajiv Subrahmanyam <rajiv@algorithms.io>
2012-09-20 08:20:48 -0700

    Minor changes, sanity checks

--- algoPricing/R/do.R
+++ algoPricing/R/do.R
@@ -25,15 +25,15 @@ convertTypes <- function(dataFrame, columnNameToTypeMap) {
 #'
 #' @param dataFrame data frame for training set
 #' @param dependentVariable the name of the dependent variable (the one we're trying to predict). It should be numeric.
-#' @param query list containing name="value" for all the known variables in the query
 #' @param inverseVariables (optional) list of numerical variables that are known to have an inverse relations (1/x). Default is no variables.
-#' @param maximalModel (optional) list of fields that can be used in a model. If a variable doesn't appear in this list, it won't be used. Default is all the columns in dataFrame.
+#' @param excludeVariables (optional) list of fields that can not be used in the model. Default no variables.
 #' @param maxFactorLevels (optional) maximum number of factor levels to be accepted for any variable in the model. Default is 500
+#' @param query list containing name="value" for all the known variables in the query
 #' @return a predicted value
 #'
 #' @author Rajiv Subrahmanyam
 #' -- need not be exported @export
-predictLM <- function(dataFrame, dependentVariable, query, inverseVariables=list(), maximalModel=names(dataFrame), maxFactorLevels=500) {
+predictLM <- function(dataFrame, dependentVariable, inverseVariables=list(), excludeVariables=list(), maxFactorLevels=500, query) {
     # First ensure that all factors in dataFrame have <= maxFactorLevels levels
     # The top maxFactorLevels levels by occurrence count will be retained and the others will be replaced with "(Other)"
     for (column in names(dataFrame)) {
@@ -43,18 +43,26 @@ predictLM <- function(dataFrame, dependentVariable, query, inverseVariables=list
     }
     formula <- paste("lm(", dependentVariable, "~")
     for (name in names(query)) {
-        if (any(maximalModel == name))  { # we want to ignore columns which are not in the maximal model
+        if (!any(excludeVariables == name))  {
+        # we want to ignore columns which are not in the maximal model
             if (is.factor(dataFrame[[name]])) {
-                if (any(query[[name]] == dataFrame[[name]])) { # query factor level exists in training data
+            # query factor level exists in training data
+                if (any(query[[name]] == levels(dataFrame[[name]]))) {
+                    query[[name]] <- as.factor(query[[name]])
                     formula <- paste(formula, name, "+")
-                } else if (any("(Other)" == dataFrame[[name]])) { # does not exist, but dataFrame has grouped factor "(Other)"
-                    query[[name]] <- as.factor("(Other)")
-                    formula <- paste(formula, name, "+")
-                } else { # else ignore factor
-                    warning(paste("Ignoring factor", name, "as it didn't appear in the training data"))
+#                } else if (any("(Other)" == levels(dataFrame[[name]]))) {
+#                # does not exist, but dataFrame has grouped factor "(Other)"
+#                    query[[name]] <- as.factor("(Other)")
+#                    formula <- paste(formula, name, "+")
+                } else {
+                # else ignore factor
+                    warning(paste("Ignoring factor", name, "as it didn't appear in the top",
+                                   maxFactorLevels, " levels of the training data"))
                 }
             } else if (is.numeric(dataFrame[[name]])) {
-                if (any(inverseVariables == name)) { # If variable has inverse relationship, add 1/x
+                query[[name]] <- as.numeric(query[[name]])
+                if (any(inverseVariables == name)) {
+                # If variable has inverse relationship, add 1/x
                     formula <- paste(formula, "I(1/", name, ") +")
                 } else {
                     formula <- paste(formula, name, "+")
@@ -63,26 +71,11 @@ predictLM <- function(dataFrame, dependentVariable, query, inverseVariables=list
         } else warning(paste("Ignoring variable", name, "as it is not in the maximal model"))
     }
     formula <- paste(substr(formula, 0, nchar(formula) - 1), ", data=dataFrame)")
+    ## At this point, if we had a braincell, we would first check to see if we have a cached trained model corresponding to these parameters
+    ## Then fall back on actually creating it. But.. sadly, teh said braincell is missing at the moment.
     linear.model <- eval(parse(text=formula))
+    print(summary(linear.model))
     prediction <- predict(linear.model, query)
-}
-
-#' Merge 2 datasets with the given join condition. Does not work!
-#'
-#' @param dataSet1 first dataset
-#' @param dataSet2 second dataset
-#' @param columnMapping a list of size 1 with key = column name in dataSet1, value = column name in dataSet2
-#' @param joinType one of 'left', 'right'
-#'
-#' @author Rajiv Subrahmanyam
-#' -- need not be exported @export
-mergeDataSets <- function(dataSet1, dataSet2, dataSet1Column, dataSet2Column, joinType) {
-    # TODO: ensure size(columnMapping) == 1
-    if (joinType == 'left') { joinCommand <- 'all.dataSet1=true' }
-    else if (joinType == 'right') { joinCommand <- 'all.dataSet2=true' }
-    col1 <-
-    merged <- merge(dataSet1, dataSet2, by.dataSet1=dataSet1Column, by.dataSet2=dataSet2Column, joinCommand)
-    merged
 }
 
 #' Suggest a price for a query based on training data from sales history.
@@ -90,19 +83,25 @@ mergeDataSets <- function(dataSet1, dataSet2, dataSet1Column, dataSet2Column, jo
 #'
 #' @param salesDataFile data file containing sales data
 #' @param priceColumn column containing price in training data
-#' @param query list with names and values of known variables in prospective sale (input parameters for pricing)
 #' @param inverseVariables (optional) list of numerical variables that are known to have an inverse relations (1/x). Default is no variables.
+#' @param excludeVariables (optional) list of variables to exclude from model
+#' @param query list with names and values of known variables in prospective sale (input parameters for pricing)
 #' @return a predicted value
 #'
 #' @author Rajiv Subrahmanyam
 #' @export
-price <- function(salesDataFile, columnNameToTypeMap, priceColumn, query, inverseVariables) {
+priceLinearPericom <- function(salesDataFile, query) {
     sales <- read.csv(salesDataFile)
+    columnNameToTypeMap <- list("item_cost"="numeric", "shipped_quantity"="numeric", "quarter_num"="factor", "Ordered_Qty_Extended_Price"="numeric", "unit_selling_price"="numeric")
     sales <- convertTypes(sales, columnNameToTypeMap)
-    prediction <- predictLM(dataFrame=sales,
-                    dependentVariable=priceColumn,
-                    query=query,
-                    inverseVariables=inverseVariables)
+    # don't allow all columns in query
+    query <- subset(query, subset=names(query) %in% list("Martket_Segment", "Product_Line", "Product_Family", "Technology",
+                            "Sold_To", "End_Customer", "Final_Customer", "Internal_Part_number", "quarter_num", "Ordered_Qty_Extended_Price",
+                            "shipped_quantity", "ASM_Region", "segment1.Territory", "segment3.Region", "item_cost"))
+    price <- max(predictLM(dataFrame=sales,
+                    dependentVariable="unit_selling_price",
+                    inverseVariables="shipped_quantity",
+                    query=query), 0) # don't want to return negative prices
 }
-# example invocation:
-# price(salesDataFile="salesOrders_081712.csv", columnNameToTypeMap = list("item_cost"="numeric", "quarter_num"="numeric", "shipped_quantity"="numeric", "quarter_num"="factor", "Ordered_Qty_Extended_Price"="numeric", "unit_selling_price"="numeric"), priceColumn="unit_selling_price", query=list(Product_Line="Clock n SAW Oscillators", Technology="XTL MTL TF", item_cost=1, shipped_quantity=100000, ASM_Region="Europe Distributor"), inverseVariables="shipped_quantity")
+# sample invocation:
+# priceLinearPericom(salesDataFile="salesOrders_081712.csv", query=list(Martket_Segment = "AUDIO", Product_Line = "Clock n SAW Oscillators", Technology = "CSO SNGL CER CNV", Sold_To = "AVNET EUROPE COMM VA", Internal_Part_number = "FD5000032", quarter_num = "4", Ordered_Qty_Extended_Price = "1960", shipped_quantity = "4000", ASM_Region = "Europe Distributor", item_cost = ".41926"))
