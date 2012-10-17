06bea432d3eb61cad0c8d950a6a82d9767828a03
Rajiv Subrahmanyam <rajiv@algorithms.io>
2012-10-17 07:07:08 -0700

    Adding the ability to evaluate any R text

--- algo2r/R/do.R
+++ algo2r/R/do.R
@@ -42,6 +42,7 @@ listFiles <- function( authToken )
 #' @param type string authToken
 #' @return the full filepath the file is saved locally to THIS server
 #' @author Robert I.
+#' @author Rajiv Subrahmanyam
 #' @export
 getFile <- function( datasetID, authToken , algoServer = "https://v1.api.algorithms.io/" )
 {
@@ -90,19 +91,11 @@ getFile <- function( datasetID, authToken , algoServer = "https://v1.api.algorit
 		}
 		else {
 			i <- sample(1:999999999,1,replace=T)
-			#print("increasing");
 		}
 	}
 
-#pre-parse the content to remove the HTTP headers
-#000000B0                    0D 0A 0D 0A                          ....
-	#
-	textOutput <<- rawToChar(content);
-	a <- fromJSON(textOutput)
-	textOutput <- gsub('\\n', '\n', a[[1]][["data"]])
+	textOutput <- gsub('\\n', '\n', fromJSON(rawToChar(content))[[1]][["data"]])
 	
-	#indexOfHTTPHeader <- regexpr("\r\n\r\n", textOutput)[1]
-	#writeLines(substr(textOutput,indexOfHTTPHeader + 4,nchar(textOutput)), destfile)
 	writeLines(textOutput, destfile)
 	echoBack <- destfile;
 	return(echoBack);
@@ -111,6 +104,7 @@ getFile <- function( datasetID, authToken , algoServer = "https://v1.api.algorit
 #' Gets a datafile from the Data Warehouse and executes the R function then removes the datafile
 #' 
 #' @param authToken (type=String) your authorization token linked to your user account
+#' @param type (type=String) one of "static" or "dynamic". If type == static, the next argument package is loaded from disk. If type == dynamic, package is treated as raw program text to be evaluated
 #' @param package (type=String) The name of the package your R function is in, you can use yourpackage to test this
 #' @param fun (type=String) The name of the function in the package you want to call, you can use yourfunction1 to test this
 #' @param datasets (type = named list) The datafiles that your R script requires, eg: list(dictionaryFile="1234", dictionaryFile2="1235" )
@@ -119,29 +113,21 @@ getFile <- function( datasetID, authToken , algoServer = "https://v1.api.algorit
 #' @author Robert I.
 #' @author Rajiv Subrahmanyam
 #' @export
-openCPUExecute <- function( authToken, algoServer = "https://v1.api.algorithms.io/" , package, fun, datasets = list(), ...) {
-	datasets <- as.list(datasets)
-	y <- list()
-	for (dataSetName in names(datasets)) {
-	    dataSet <- datasets[[dataSetName]]
-            y[[dataSetName]] <- as.character(lapply(dataSet, getFile, authToken, algoServer))
-	}
-	params <- append(y, list(...))
-	library(package,character.only=TRUE);
-	proxyOutput <- do.call(fun, params);
-	
-	#for ( additional_files in y )
-	#{
-	#	if ( file.exists(additional_files) ){
-	#	file.remove(additional_files)
-	#	}
-	#	else
-	#	{
-	#		if (debug)
-	#		{
-	#			print("odd, the additional file is not found");
-	#			print(additional_files);
-	#		}
-	#	}
-	#}
+openCPUExecute <- function(authToken, algoServer = "https://v1.api.algorithms.io/" , type = "static", package, fun, datasets = list(), ...) {
+    datasets <- as.list(datasets)
+    y <- list()
+    for (dataSetName in names(datasets)) {
+        dataSet <- datasets[[dataSetName]]
+        y[[dataSetName]] <- as.character(lapply(dataSet, getFile, authToken, algoServer))
+    }
+    params <- append(y, list(...))
+    if (type == "static") {
+        library(package,character.only=TRUE);
+    } else if (type == "dynamic") {
+        eval(parse(package))
+    } else {
+        stop('type must be one of "static", "dynamic"')
+    }
+
+    proxyOutput <- do.call(fun, params);
 }
