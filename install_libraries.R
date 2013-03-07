7a2d9ce7c121465cd5b57d8df7425ce95c262338
Rajiv Subrahmanyam <rajiv@algorithms.io>
2013-03-07 08:30:11 -0800

    checking in required libs

---
+++ install_libraries.R
@@ -0,0 +1,7 @@
+install.packages(c("ada", "arules", "bitops", "cba",
+       "combinat", "doBy", "ellipse", "fBasics", "fpc",
+       "gdata", "gplots", "gtools", "Hmisc", "kernlab",
+       "MASS", "network", "randomForest", "rggobi", "RODBC",
+       "ROCR", "rpart", "XML", "RCurl", "digest", "plotrix",
+       "plyr", "ggplot2", "gsubfn", "RJSONIO", "conf.design",
+        "tm", "tree"))
