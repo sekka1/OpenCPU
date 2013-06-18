a1f1905677abbbba3250b54b3b0f41bfda3ced33
Anthony Yee <smyee@yahoo.com>
2013-06-18 16:35:19 -0700

    updated to be the same as in Algoio repo

--- algoio/R/do.R
+++ algoio/R/do.R
@@ -33,19 +33,104 @@ read.description <- function(file) {
 #' @export
 algoio.publish <- function(package.dir) {
   swagger <- generate_swagger(package.dir)
+  # Create an json file prefixed with the package name
+  jsonfile <- paste0(swagger$package$Package, 'swagger.json')
+  sink(jsonfile)
+  cat(toJSON(swagger))
+  sink()
+  
   system(paste('R CMD build ', normalizePath(package.dir)))
   # where is the package tarball
-  tarball <- list.files(pattern=paste(basename(package.dir),'.*gz'))
+  tarball <- list.files(pattern=paste0(swagger$package$Package,'.*gz'))
+  
+  if (!exists('authToken')) 
+    authToken <- getinput("Authentication Token")
+  if (!exists('algoServer'))
+    algoServer <- getinput('Algorithms.io Server', defvalue='https://v1.api.algorithms.io/')
+  
+  content <- executeAPICall(authToken, algoServer, 
+                            'packages', action=postForm, 
+                            definition=toJSON(swagger), package=fileUpload(tarball))
+  algoid = fromJSON(content) 
+  print(paste("Successfully uploaded package", basename(package.dir), ", reference is", algoid))
+  cat(fromJSON(sub(".*(\\{\"apiVersion.*)", "\\1", content)))
 }
 
-#' Upload dataset to algorithms.io 
-algoio.upload <- function(file.path) {
+algoio.show <- function(package.id) {
+  if (!exists('authToken')) 
+    authToken <- getinput("Authentication Token")
+  if (!exists('algoServer'))
+    algoServer <- getinput('Algorithms.io Server', defvalue='https://v1.api.algorithms.io/')
+  content <- executeAPICall(
+    authToken, 
+    "http://pod3.staging.www.algorithms.io/", 
+    paste0('catalog/swagger/id/',package.id))
+  return(sub(".*(\\{\"apiVersion.*)", "\\1", content))
+}
+
+algoio.run <- function(algo.id) {
+  if (!exists('authToken')) 
+    authToken <- getinput("Authentication Token")
+  if (!exists('algoServer'))
+    algoServer <- getinput('Algorithms.io Server', defvalue='https://v1.api.algorithms.io/')
+  content <- executeAPICall(
+    authToken, 
+    algoServer, 
+    paste0('jobs/swagger/id/', algo.id),
+    action=postForm)
+  return(sub(".*(\\{\"apiVersion.*)", "\\1", content))
+}
+
+#' Upload R data.frame to algorithms.io 
+#' @description Upload data.frame to algorithms.io
+#' @param x @@type=data.frame
+#' @keywords dataframe
+#' @export 
+algoio.upload.dataframe <- function(x) {
+  stopifnot(is.data.frame(x))
+  require("RJSONIO")
   
+  if (!exists('authToken')) 
+    authToken <- getinput("Authentication Token")
+  if (!exists('algoServer'))
+    algoServer <- getinput('Algorithms.io Server', defvalue='https://v1.api.algorithms.io/')
+  
+  # 
+  dir.create(path='~/.algoio', showWarnings=FALSE, recursive=TRUE)
+  file.create('~/.algoio/dataset')
+  write.table(x, '~/.algoio/dataset')
+
+  content <- executeAPICall(authToken, algoServer, 'dataset', action=postForm, theFile=fileUpload('~/.algoio/dataset'))
+  json <- fromJSON(content)[[1]]
+  if (json$api[['Authentication']] == 'Success')
+    print(paste('Uploaded data frame with returned reference', json$data))
+}
+
+algoio.delete.dataset <- function(id) {
+  require(stringr)
+  if (!exists('authToken')) 
+    authToken <- getinput("Authentication Token")
+  if (!exists('algoServer'))
+    algoServer <- getinput('Algorithms.io Server', defvalue='https://v1.api.algorithms.io/')
+  content <- executeAPICall(authToken, algoServer, "/dataset");
+  dataset <- fromJSON(str_extract(content, "\\{.*\\}"))$data
+  if (id %in% sapply(dataset,'[[','id'))
+    response <- executeAPICall(authToken, algoServer, paste0('dataset/',id), action=httpDELETE)
+  return(response)
+}
+
+makeCache <- function() {
+  cache <- new.env(parent=emptyenv())
+  list(get = function(key) cache[[key]],
+       set = function(key, value) cache[[key]] <- value,
+       ## next two added in response to @sunpyg
+       load = function(rdaFile) load(rdaFile, cache),
+       ls = function() ls(cache))
 }
 
 #' Get package level data from DESCRIPTION file
 get_package_data <- function(package.dir) {
-  library('stringr')
+  require('stringr')
   # If description present, use Collate to order the files
   # (but still include them all, and silently remove missing)
   DESCRIPTION <- file.path(package.dir, "DESCRIPTION")
@@ -70,10 +155,11 @@ get_package_data <- function(package.dir) {
 #' Generates a swagger document from a package directory
 #' @param package.dir the package's top directory
 generate_swagger <- function(package.dir) {
-    library("roxygen2")
-    library("RJSONIO")
+    require("roxygen2")
+    require("RJSONIO")
 
-    swagger <- fromJSON('doc/swagger.json')
+    swagger <- fromJSON(getURL('https://s3.amazonaws.com/r-package/algoio.swagger.json'))
+    # swagger <- fromJSON('doc/swagger.json')
     r_files <- dir(file.path(package.dir, "R"), "[.Rr]$", full.names = TRUE)
     parsed <- parse.files(r_files)
     first <- TRUE
@@ -106,13 +192,25 @@ generate_swagger <- function(package.dir) {
     return(swagger)
 }
 
-#' Sample function used for testing publish
-#' @param param1 string parameter
-#' @param param2 string parameter with null default
-#' @param param3 numeric parameter
-#' @author anonymous@@algorithms.io
-#' @export
-#' @keywords foo, bar
-algoio.foobar <- function(param1="default string value", param2=NULL, param3=123) {
-    list()
+#' Calls a URL on the API site with the given relative path and action
+executeAPICall <- function(authToken, algoServer="https://v1.api.algorithms.io/", relativePath, action=getURL, ...) {
+  require(RCurl)
+  CAINFO = paste(system.file(package="RCurl"), "/CurlSSL/ca-bundle.crt", sep = "")
+  
+  url <- paste(algoServer,relativePath,sep="");
+  theheader <- c('authToken' = authToken);
+  #note the cookie might cause some trouble with apparmor
+  cookie <- 'cookiefile.txt'
+  curlH <- getCurlHandle(
+    cookiefile = cookie,
+    useragent =  "Mozilla/5.0 (Windows; U; Windows NT 5.1; en - US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6",
+    header = TRUE,
+    verbose = FALSE,
+    netrc = TRUE,
+    maxredirs = as.integer(20),
+    followlocation = TRUE,
+    ssl.verifypeer = FALSE,
+    httpheader = theheader
+  );
+  return(action(url, curl = curlH, cainfo = CAINFO, ...));
 }
