df83c72f409501f57c94a5e16a049e1db0ef5edf
Anthony Yee <smyee@yahoo.com>
2013-07-27 15:24:38 -0700

    initial pagerank results based on crunchbase data

--- neo4j-r/R/do.R
+++ neo4j-r/R/do.R
@@ -19,9 +19,14 @@ queryCypher <- function(querystring) {
   data
 }
 
-#' 
-#'
-#'
+#' Executes cypher query
+#' @description Executes cypher query to Neo4j server instance
+#' @param serverURL URL of Neo4j cypher query endpoint.  It should end with db/data/cypher
+#' @param querystring Query
+#' @export
+#' @import RCurl, RJSONIO, bitops
+#' @include RCurl, RJSONIO, bitops
+#' @examples queryCypher2("match (e)-[:HAS_EMPLOYMENT]->(j) return e,j limit 10")
 #'
 queryCypher2 <- function(serverURL="http://166.78.24.138:7474/db/data/cypher", 
                          querystring) {
@@ -84,13 +89,13 @@ weightByTitle <- function(title) {
     .05;
   }
   else if (grepl("advisor",title)) {
-    .05;
+    .01;
   }
   else if (grepl("chairman",title)) {
     .15;
   }
   else if (grepl("board member",title) || grepl("board+member",title)) {
-    .1;
+    .01;
   }
   else if (grepl("partner",title)) {
     .05;
@@ -130,13 +135,14 @@ getScore <- function(moneyString) {
   }
 }
 
-#' Creates data frame with these columns
+#' Score persons  
+#' @description Score is based on the total money raised of a company, discounted according to person's job title.  
+#' @return a data.frame with columns "person", "coworker" and "score"
 #'
-#'
-#'
-scoreAllPersons <- function() {
-  persons <- queryCypher2(querystring="match (e)-[:HAS_EMPLOYMENT]->(j)-[:HAS_EMPLOYMENT_FIRM]->(f),(j)-[:HAS_EMPLOYMENT_TITLE]->(t),(f)<-[:HAS_EMPLOYMENT_FIRM]-k<-[:HAS_EMPLOYMENT]-(p) where HAS(f.total_money_raised) and f.total_money_raised <> \"$0\" return e.source_uid, f.value, t.value, count(distinct p), f.total_money_raised order by e.source_uid;")
-  names(persons) <- c("person", "company", "title", "employees_on_base_crunchbase", "total_money_raised")
+scoreCrunchBase <- function() {
+  library(stringr)
+  persons <- queryCypher2(querystring="match (e)-[:HAS_EMPLOYMENT]->(j)-[:HAS_EMPLOYMENT_FIRM]->(f),(j)-[:HAS_EMPLOYMENT_TITLE]->(t),(f)<-[:HAS_EMPLOYMENT_FIRM]-k<-[:HAS_EMPLOYMENT]-(p) where HAS(f.total_money_raised) and f.total_money_raised <> \"$0\" return e.source_uid, f.value, t.value, p.source_uid, f.total_money_raised limit 10000;")
+  names(persons) <- c("person", "company", "title", "coworker", "total_money_raised")
   persons$score <- sapply(persons[,"total_money_raised"], function(x) str_extract(x,"[0-9.]+"))
   exchange <- c("$"=1.0, "¥"=.01, "£"=1.5, "€"=1.3, "c$"=0.95)
   unit <- c("m"=1e6, "k"=1e3, "b"=1e9)
@@ -144,4 +150,42 @@ scoreAllPersons <- function() {
   persons[,"score"] <- persons[,"score"] * sapply(persons[,"title"], weightByTitle)
   persons
 }
+
+#'
+#' Create igraph from relations
+#' @description Create igraph out of all persons in neo4j db
+#' @param relations a data.frame with at least 3 columns, person, related and score
+#' @export
+#'
+createGraph <- function(relations) {
+  library(igraph)
+  library(bitops)
+  # related <- queryCypher2(querystring="match (p)-[:HAS_EMPLOYMENT]->j-[:HAS_EMPLOYMENT_FIRM]->(f)<-[:HAS_EMPLOYMENT_FIRM]-k<-[:HAS_EMPLOYMENT]-(q) return p.source_uid, q.source_uid limit 100;")
+  if (!is.data.frame(relations)) {
+    write("relations need to be a dataframe", stderr())
+    return
+  }
+   
+  g <- graph.edgelist(as.matrix(persons[,c("person", "coworker")]))
+  E(g)$weight <- persons[,"score"]
+  
+  plot.igraph(g)
+  g
+}
+
+#'
+#' #' Run page rank on igraph from CrunchBase
+#' @description Run igraph page rank algorithm on graph with scores from Crunchbase data, show the top entries with scores
+#' @export
+#' 
+runPageRank <- function(top=20) {
+  graph <- createGraph(scoreCrunchBase())
+  vector <- page.rank(graph)$vector
+  head(sort(vector, decreasing=T),n=top)
+  vector
+}
+
+
+
+
 
